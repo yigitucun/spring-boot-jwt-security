25640606c9f1978c4961f414410a0fb913c1eb5f
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 18:47:12 +0300

    Update Filter.java

--- src/main/java/com/ali/authenticationservice/filters/Filter.java
+++ src/main/java/com/ali/authenticationservice/filters/Filter.java
@@ -1,10 +1,13 @@
 package com.ali.authenticationservice.filters;
 
+import com.ali.authenticationservice.core.utils.JwtService;
+import com.auth0.jwt.interfaces.Claim;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
@@ -12,8 +15,47 @@ import java.io.IOException;
 @Service
 @AllArgsConstructor
 public class Filter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-      filterChain.doFilter(request,response);
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String token = authHeader.substring(7);
+        if (!jwtService.validateToken(token)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        Claim username = jwtService.getClaimFromToken("username");
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+        }
+        filterChain.doFilter(request,response);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
