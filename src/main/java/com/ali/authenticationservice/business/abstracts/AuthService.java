4ce9b3060f81bfcdbadf517f6ca4d8c5a3a4ac9b
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 19:58:38 +0300

    Update AuthService.java

--- src/main/java/com/ali/authenticationservice/business/abstracts/AuthService.java
+++ src/main/java/com/ali/authenticationservice/business/abstracts/AuthService.java
@@ -1,7 +1,10 @@
 package com.ali.authenticationservice.business.abstracts;
 
 import com.ali.authenticationservice.dto.requests.AddAuthRequest;
+import com.ali.authenticationservice.dto.requests.AuthRequest;
+import com.ali.authenticationservice.dto.responses.TokenResponse;
 
 public interface AuthService {
     AddAuthRequest add(AddAuthRequest request);
+    TokenResponse authenticate(AuthRequest request);
 }
