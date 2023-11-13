a35771a65a2c8f81eb3c731d0e0a49c052357ec4
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 20:20:07 +0300

    Update AddRoleRequest.java

--- src/main/java/com/ali/authenticationservice/dto/requests/AddRoleRequest.java
+++ src/main/java/com/ali/authenticationservice/dto/requests/AddRoleRequest.java
@@ -1,11 +1,12 @@
 package com.ali.authenticationservice.dto.requests;
 
 import com.ali.authenticationservice.entities.concretes.Role;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AddRoleRequest {
-
+    @NotBlank(message = "Bu alan zorunludur.")
     private String role;
     public Role toEntity(AddRoleRequest request){
         return Role.builder()
