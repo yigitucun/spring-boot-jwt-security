d18bd88e418d66e66428fc21bfd1f0c4a6adf0f6
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 18:20:50 +0300

    create user repository
    create user repository

---
+++ src/main/java/com/ali/authenticationservice/dataAccess/abstracts/IUserRepository.java
@@ -0,0 +1,11 @@
+package com.ali.authenticationservice.dataAccess.abstracts;
+
+import com.ali.authenticationservice.entities.concretes.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User,Integer> {
+    boolean existsById(int id);
+    User findUserByUsername(String username);
+}
