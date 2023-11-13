6af122efdfa73b7e47fd7ac07949581ff46b48ce
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 18:26:26 +0300

    Update Role.java

--- src/main/java/com/ali/authenticationservice/entities/concretes/Role.java
+++ src/main/java/com/ali/authenticationservice/entities/concretes/Role.java
@@ -2,6 +2,7 @@ package com.ali.authenticationservice.entities.concretes;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
