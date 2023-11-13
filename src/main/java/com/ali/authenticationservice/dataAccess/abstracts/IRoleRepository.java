30d88a6c7cba8995e14351e34d4373a7f89937d1
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 18:20:39 +0300

    create role repository
    create role repository

---
+++ src/main/java/com/ali/authenticationservice/dataAccess/abstracts/IRoleRepository.java
@@ -0,0 +1,12 @@
+package com.ali.authenticationservice.dataAccess.abstracts;
+
+import com.ali.authenticationservice.entities.concretes.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<Role,Integer> {
+    boolean existsById(int id);
+    Role findById(int id);
+
+}
