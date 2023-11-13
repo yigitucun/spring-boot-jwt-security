c9d0be184973bb45d7daad6961249519363f24a5
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-14 00:14:10 +0300

    Update JwtService.java

--- src/main/java/com/ali/authenticationservice/core/utils/JwtService.java
+++ src/main/java/com/ali/authenticationservice/core/utils/JwtService.java
@@ -1,6 +1,7 @@
 package com.ali.authenticationservice.core.utils;
 
 
+import com.ali.authenticationservice.entities.concretes.Role;
 import com.ali.authenticationservice.entities.concretes.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
@@ -12,24 +13,26 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
     Algorithm algorithm = Algorithm.HMAC256("secret");
-    DecodedJWT jwt;
     public String generateToken(User user){
+        Date now = new Date();
         return JWT.create()
                 .withIssuer("auth0")
                 .withIssuedAt(new Date())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 50000L))
+                .withExpiresAt(new Date(now.getTime() + 2 * 60 * 60 * 1000))
                 .withClaim("username",user.getUsername())
                 .withClaim("name",user.getName())
-                .withClaim("roles",user.getRoles())
+                .withClaim("roles",user.getRoles().stream().map(Role::getRole).collect(Collectors.toList()))
                 .sign(algorithm);
     }
 
-    public Claim getClaimFromToken(String from){
-        return jwt.getClaim(from);
+    public Claim getClaimFromToken(String token,String from){
+        DecodedJWT decodedJWT = JWT.decode(token);
+        return decodedJWT.getClaim(from);
     }
     public boolean validateToken(String token){
         try{
