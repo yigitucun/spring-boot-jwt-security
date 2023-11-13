23e3d9b6502210a57d3163e3c0502732f6a05c11
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 18:03:22 +0300

    create role entitiy
    create role entitiy

--- src/main/java/com/ali/authenticationservice/entities/concretes/Role.java
+++ src/main/java/com/ali/authenticationservice/entities/concretes/Role.java
@@ -1,4 +1,18 @@
 package com.ali.authenticationservice.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String role;
 }
