812e47337ce9d049b518f29599229f66b13666a9
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 20:10:47 +0300

    Update AuthController.java

--- src/main/java/com/ali/authenticationservice/api/controllers/AuthController.java
+++ src/main/java/com/ali/authenticationservice/api/controllers/AuthController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -25,5 +22,10 @@ public class AuthController {
     @PostMapping("/authenticate")
     public ResponseEntity<?> authenticate(@RequestBody AuthRequest request){
         return new ResponseEntity<>(this.authService.authenticate(request),HttpStatus.CREATED);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsers(){
+        return ResponseEntity.ok(this.authService.getUsers());
     }
 }
