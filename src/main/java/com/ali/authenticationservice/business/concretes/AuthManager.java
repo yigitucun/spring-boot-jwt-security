6a238704f8c3073e42c0ae6ff0cb8285551105b8
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 19:58:35 +0300

    Update AuthManager.java

--- src/main/java/com/ali/authenticationservice/business/concretes/AuthManager.java
+++ src/main/java/com/ali/authenticationservice/business/concretes/AuthManager.java
@@ -2,9 +2,15 @@ package com.ali.authenticationservice.business.concretes;
 
 import com.ali.authenticationservice.business.abstracts.AuthService;
 import com.ali.authenticationservice.business.rules.UserBusinessRules;
+import com.ali.authenticationservice.core.utils.JwtService;
 import com.ali.authenticationservice.dataAccess.abstracts.IUserRepository;
 import com.ali.authenticationservice.dto.requests.AddAuthRequest;
+import com.ali.authenticationservice.dto.requests.AuthRequest;
+import com.ali.authenticationservice.dto.responses.TokenResponse;
+import com.ali.authenticationservice.entities.concretes.User;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +21,8 @@ public class AuthManager  implements AuthService {
     private IUserRepository userRepository;
     private UserBusinessRules userBusinessRules;
     private PasswordEncoder encoder;
+    private JwtService jwtService;
+    private AuthenticationManager authenticationManager;
 
     @Override
     public AddAuthRequest add(AddAuthRequest request) {
@@ -23,4 +31,37 @@ public class AuthManager  implements AuthService {
         this.userRepository.save(request.toEntity(request));
         return request;
     }
+
+    @Override
+    public TokenResponse authenticate(AuthRequest request) {
+        User user = this.userRepository.findByUsername(request.getUsername());
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        String token = jwtService.generateToken(user);
+        TokenResponse response = new TokenResponse();
+        response.setToken(token);
+        return response;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
