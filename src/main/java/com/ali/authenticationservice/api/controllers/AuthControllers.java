031085b19d3c3dbe2e4d5f6368ef4f26dade6d68
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-13 18:33:55 +0300

    Create AuthControllers.java

---
+++ src/main/java/com/ali/authenticationservice/api/controllers/AuthControllers.java
@@ -0,0 +1,22 @@
+package com.ali.authenticationservice.api.controllers;
+
+import com.ali.authenticationservice.business.abstracts.AuthService;
+import com.ali.authenticationservice.dto.requests.AddAuthRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@AllArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthControllers {
+    private AuthService authService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@Valid @RequestBody AddAuthRequest request){
+        return new ResponseEntity<>(this.authService.add(request), HttpStatus.CREATED);
+    }
+}
