d97db2b13e56f922e438f11f1803e65ddea64d94
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-18 22:57:43 +0300

    Update Security.java

--- src/main/java/com/ali/authenticationservice/security/Security.java
+++ src/main/java/com/ali/authenticationservice/security/Security.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,6 +32,7 @@ public class Security {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize->authorize
                         .requestMatchers("/api/auth/users").permitAll()
                         .anyRequest().permitAll()
@@ -41,7 +43,6 @@ public class Security {
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .authenticationProvider(this.authenticationProvider());
 
-        http.csrf(csrf->csrf.disable());
         return http.build();
     }
 
